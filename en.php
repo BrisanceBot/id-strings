d2e0cdfd190817294bb1ccbedfce8bc446e1af49
Bye <bye@byecorps.com>
2024-06-28 22:18:11 +0100

    That could break things

--- en.php
+++ en.php
@@ -1,5 +1,3 @@
 <?php
-$LANG['generic']['generic'] = 'You\'ve unlocked the generic string!';
-
 $LANG['auth']['signup'] = 'Sign up';
 $LANG['generic']['generic'] = 'You\'ve unlocked the generic text!';
