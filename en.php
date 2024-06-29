980beb66b21d9f08e7edc15f4a5a7fdeacd960a4
Bye <bye@byecorps.com>
2024-06-29 12:09:24 +0100

    Add error messages

--- en.php
+++ en.php
@@ -2,3 +2,5 @@
 $LANG['auth']['signup'] = 'Sign up';
 $LANG['generic']['generic'] = 'You\'ve unlocked the generic text!';
 $LANG['generic']['error'] = 'An error occurred.';
+$LANG['error']['generic'] = 'No error message provided.';
+$LANG['error']['loggedIn'] = 'You\'re already logged in.';
