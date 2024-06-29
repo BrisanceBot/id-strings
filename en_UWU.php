febb98cd93b6fbccbdb7c9a2e9f5293acb965790
Hakase <hakase@litdevs.org>
2024-06-29 11:21:49 +0000

    Translated using Weblate (Cute Engwish (en_UWU))
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: ByeCorps ID/Web
    Translate-URL: https://translit.litdevs.org/projects/byecorps-id/web/en_UWU/

--- en_UWU.php
+++ en_UWU.php
@@ -1,6 +1,7 @@
 <?php
-$LANG['auth']['signup'] = '';
-$LANG['generic']['generic'] = '';
+$LANG['auth']['signup'] = 'myake user ^~^';
+$LANG['generic']['generic'] = 'u got da boring text :D';
 $LANG['generic']['error'] = 'Ohh nyo an ewwow 3:';
 $LANG['error']['generic'] = 'nyo ewwow massage :P';
 $LANG['error']['loggedIn'] = 'u r logged in already siwwy :3';
+$LANG['error']['notLoggedIn'] = 'u r not logged in ;~;';
