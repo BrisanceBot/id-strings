be09ec30ea84d19483d11a824af5aa8ea206f809
bye <bye@byecorps.com>
2024-06-29 22:53:19 +0100

    New strings

--- en.php
+++ en.php
@@ -1,5 +1,8 @@
 <?php
 $LANG['auth']['signup'] = 'Sign up';
+$LANG['auth']['login'] = 'Log in';
+$LANG['page']['credits'] = 'Credits';
+$LANG['footer']['executionTime'] = 'Execution time';
 $LANG['generic']['generic'] = 'You\'ve unlocked the generic text!';
 $LANG['generic']['error'] = 'An error occurred.';
 $LANG['error']['generic'] = 'No error message provided.';
