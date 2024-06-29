2e6dd7725ceb15aeb67a08d47f8dee6a2077cc1a
Bye <bye@byecorps.com>
2024-06-29 12:19:07 +0100

    Update en.php

--- en.php
+++ en.php
@@ -4,3 +4,4 @@ $LANG['generic']['generic'] = 'You\'ve unlocked the generic text!';
 $LANG['generic']['error'] = 'An error occurred.';
 $LANG['error']['generic'] = 'No error message provided.';
 $LANG['error']['loggedIn'] = 'You\'re already logged in.';
+$LANG['error']['notLoggedIn'] = 'You\'re not logged in';
