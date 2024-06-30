72a48e1996e05d87f2f6e97c811c6cc68222d1b3
Weblate <noreply@weblate.org>
2024-07-01 00:25:52 +0300

    Update translation files
    Updated by "Remove blank strings" hook in Weblate.
    
    Translation: ByeCorps ID/Web
    Translate-URL: https://translit.litdevs.org/projects/byecorps-id/web/

--- en_US.php
+++ en_US.php
@@ -1,10 +1,1 @@
 <?php
-$LANG['auth']['signup'] = '';
-$LANG['auth']['login'] = '';
-$LANG['page']['credits'] = '';
-$LANG['footer']['executionTime'] = '';
-$LANG['generic']['generic'] = '';
-$LANG['generic']['error'] = '';
-$LANG['error']['generic'] = '';
-$LANG['error']['loggedIn'] = '';
-$LANG['error']['notLoggedIn'] = '';
