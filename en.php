729d95f1310a930ab57c8983d9c35cc63d8f233d
bye <bye@byecorps.com>
2024-07-01 22:15:35 +0100

    New strings!

--- en.php
+++ en.php
@@ -1,17 +1,26 @@
 <?php
 $LANG['auth']['signup'] = 'Sign up';
 $LANG['auth']['login'] = 'Log in';
+$LANG['auth']['signout'] = 'Sign out';
+$LANG['auth']['signedout'] = 'You were signed out.';
 $LANG['auth']['email'] = 'Email';
 $LANG['auth']['password'] = 'Password';
 $LANG['auth']['confirmPassword'] = 'Confirm password';
+
 $LANG['button']['submit'] = 'Submit';
+
 $LANG['page']['credits'] = 'Credits';
 $LANG['page']['signup'] = 'Sign up for ByeCorps ID';
 $LANG['page']['login'] = 'Log in to ByeCorps ID';
+
 $LANG['footer']['executionTime'] = 'Execution time';
+
 $LANG['generic']['generic'] = 'You\'ve unlocked the generic text!';
 $LANG['generic']['error'] = 'An error occurred.';
+
 $LANG['error']['generic'] = 'No error message provided.';
 $LANG['error']['loggedIn'] = 'You\'re already logged in.';
 $LANG['error']['notLoggedIn'] = 'You\'re not logged in';
+$LANG['error']['incorrectAuth'] = 'Incorrect username or password.';
+
 $LANG['name'] = 'English (Traditional)';
