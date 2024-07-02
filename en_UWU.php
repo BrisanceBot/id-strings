02db9d00fc4b16c6acdb62acb724c05bf198e7fb
Bye <bye@omg.lol>
2024-07-02 11:14:20 +0000

    Translated using Weblate (kawaii engwish ^_^ (en_UWU))
    Currently translated at 50.0% (10 of 20 strings)
    
    Translation: ByeCorps ID/Web
    Translate-URL: https://translit.litdevs.org/projects/byecorps-id/web/en_UWU/

--- en_UWU.php
+++ en_UWU.php
@@ -1,6 +1,6 @@
 <?php
 $LANG['auth']['signup'] = 'myake user ^~^';
-$LANG['generic']['generic'] = 'u got da boring text :D';
+$LANG['generic']['generic'] = 'u bwoke it :3';
 $LANG['generic']['error'] = 'Ohh nyo an ewwow 3:';
 $LANG['error']['generic'] = 'nyo ewwow massage :P';
 $LANG['error']['loggedIn'] = 'u r logged in already siwwy :3';
@@ -8,6 +8,7 @@ $LANG['error']['notLoggedIn'] = 'u r not logged in ;~;';
 $LANG['page']['signup'] = 'get a bye-corpse passpowt :D';
 $LANG['name'] = 'kawaii engwish ^_^';
 $LANG['page']['login'] = 'let me haz ur bye-corpse passpowt OwO';
-$LANG['footer']['executionTime'] = 'pooping time';
+$LANG['footer']['executionTime'] = '';
 $LANG['button']['submit'] = 'submit >///<';
 $LANG['error']['incorrectAuth'] = 'u typed ur login wrong :P';
+$LANG['header']['hello'] = 'omg hai {{display_name}}~~!!';
