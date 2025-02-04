02b14da5dbf8b854592386cd4deaae268a5c868b
Bye <bye@byecorps.com>
2025-02-04 10:21:49 +0000

    Translated using Weblate (Irish)
    Currently translated at 20.4% (9 of 44 strings)
    
    Translation: ByeCorps ID/Web
    Translate-URL: https://translit.litdevs.org/projects/byecorps-id/web/ga/

--- ga.php
+++ ga.php
@@ -1,2 +1,10 @@
 <?php
 $LANG['name'] = 'Gaeilge';
+$LANG['auth']['email'] = 'Ríomhphost';
+$LANG['header']['hello'] = 'Haigh {{display_name}}!';
+$LANG['language']['en'] = 'Béarla';
+$LANG['language']['en_US'] = 'Béarla (Meiriceá)';
+$LANG['language']['fi'] = 'Fionlainnise';
+$LANG['language']['ga'] = 'Gaeilge';
+$LANG['language']['nb_NO'] = 'Ioruais';
+$LANG['language']['en_UWU'] = 'Béarla Gleoite';
