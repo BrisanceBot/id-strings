72c7e3e6984d0867669fa741bc0f0968d0fbaddd
Bye <bye@byecorps.com>
2025-02-17 16:59:48 +0000

    Translated using Weblate (kawaii engwish :3 (en_UWU))
    Currently translated at 29.7% (14 of 47 strings)
    
    Translation: ByeCorps ID/Web
    Translate-URL: https://translit.litdevs.org/projects/byecorps-id/web/en_UWU/

--- en_UWU.php
+++ en_UWU.php
@@ -1,14 +1,15 @@
 <?php
 $LANG['auth']['signup'] = 'myake user ^~^';
-$LANG['generic']['generic'] = 'u bwoke it :3';
-$LANG['generic']['error'] = 'Ohh nyo an ewwow 3:';
-$LANG['error']['generic'] = 'nyo ewwow massage :P';
+$LANG['generic']['generic'] = 'u unlocked da generic text!';
+$LANG['generic']['error'] = 'ohh nyo ewwow 3:';
+$LANG['error']['generic'] = 'nyo ewwow message :P';
 $LANG['error']['loggedIn'] = 'u r logged in already siwwy :3';
 $LANG['error']['notLoggedIn'] = 'u r not logged in ;~;';
-$LANG['page']['signup'] = 'get a byecorps id :D';
+$LANG['page']['signup'] = 'get a {{byecorps_id}} :D';
 $LANG['name'] = 'kawaii engwish :3';
-$LANG['page']['login'] = 'log in with ur byecorps id OwO';
+$LANG['page']['login'] = 'log in with ur {{byecorps_id}} OwO';
 $LANG['footer']['executionTime'] = '<b>dis page took me:</b> {{time}} ms!';
 $LANG['button']['submit'] = 'submit >///<';
-$LANG['error']['incorrectAuth'] = 'u typed ur login wrong :P';
-$LANG['header']['hello'] = 'omg hai {{display_name}}~~!!';
+$LANG['error']['incorrectAuth'] = 'wrong details dummy :P';
+$LANG['header']['hello'] = 'hai {{display_name}}~~!!';
+$LANG['error']['invalidEmail'] = 'wrong email dummy >:3';
